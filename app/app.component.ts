164b555e19d599cefbd122ba5f77766487cc568e
Noah <yasskin@icloud.com>
2016-08-07 21:55:35 -0700

    add components files to app and styles to css

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+import { TaskListComponent } from './task-list.component';
+import { Task } from './task.model';
 
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
     <div class="container">
-      <h1>New Skeleton Angular2 App!</h1>
+      <h1>To-Do List</h1>
+      <task-list
+        [taskList]="tasks"
+        (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
     </div>
   `
 })
 
 export class AppComponent {
+  public tasks: Task[];
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log('parent', clickedTask);
+  }
+}
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
 }
