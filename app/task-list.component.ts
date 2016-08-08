a2d49674ab3527f0417210934c6e45e9c8aefa48
Noah <yasskin@icloud.com>
2016-08-07 22:28:11 -0700

    add more component code and css styles

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -2,12 +2,13 @@ import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
+import { NewTaskComponent } from './new-task.component';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
-  directives: [TaskComponent, EditTaskDetailsComponent],
+  directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   template: `
     <task-display *ngFor="#currentTask of taskList"
       (click)="taskClicked(currentTask)"
@@ -15,6 +16,7 @@ import { EditTaskDetailsComponent } from './edit-task-details.component';
       [task]="currentTask">
     </task-display>
     <edit-task-details *ngIf="selectedTask" [task]="selectedTask"></edit-task-details>
+    <new-task></new-task>
     `
   })
 export class TaskListComponent {
@@ -23,10 +25,16 @@ export class TaskListComponent {
   public selectedTask: Task;
   constructor() {
     this.onTaskSelect = new EventEmitter();
+    <new-task (onSubmitNewTask)="createTask($event)"></new-task>
   }
   taskClicked(clickedTask: Task): void {
     console.log('child', clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
+  createTask(description: string): void {
+    this.taskList.push(
+      new Task(description, this.taskList.length)
+    );
+  }
   }
 }
